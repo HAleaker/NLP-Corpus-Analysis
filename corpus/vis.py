f7c1dee9d6099492fdac021b64cde9b2e363d528
HAleaker <99490481+HAleaker@users.noreply.github.com>
2021-03-02 08:15:16 +0000

    Reorganizing

--- corpus/vis.py
+++ corpus/vis.py
@@ -1,3 +1,19 @@
 """ Utilities to integrate with pyLDAvis
 
-Most of the code lifted from pyLDAvis.skle
+Most of the code lifted from pyLDAvis.sklearn, only difference is how we extract the vocabulary.
+"""
+
+
+import funcy as fp
+import pyLDAvis as lda
+
+
+def _get_doc_lengths(dtm):
+    return dtm.sum(axis=1).getA1()
+
+
+def _get_term_freqs(dtm):
+    return dtm.sum(axis=0).getA1()
+
+
+def _row_norm(dist
