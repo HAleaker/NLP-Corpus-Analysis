5b38e621fc87418439c82ce7927fb90c30f66823
HAleaker <99490481+HAleaker@users.noreply.github.com>
2022-07-23 15:56:23 +0000

    Bump to v1.1.1

---
+++ src/eea.corpus/eea/corpus/__init__.py
@@ -0,0 +1,37 @@
+
+from pyramid.config import Configurator
+from pyramid.session import SignedCookieSessionFactory
+
+import matplotlib
+
+matplotlib.use('SVG')       # use SVG backend.
+
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    secret = settings['corpus.secret']
+    session_factory = SignedCookieSessionFactory(secret)
+
+    config = Configurator(settings=settings)
+    config.set_session_factory(session_factory)
+
+    config.include('pyramid_chameleon')
+
+    config.add_static_view('static', 'static', cache_max_age=3600)
+
+    config.add_route('home', '/')
+    config.add_route('upload_csv', '/upload')
+    config.add_route('corpus_view', '/view/{doc}/{corpus}/{page}')
+    config.add_route('corpus_topics', '/topics/{doc}/{corpus}')
+    config.add_route('delete_corpus', '/delete/{doc}/{corpus}')
+    config.add_route('process_csv', '/process/{doc}/')
+    config.add_route('corpus_classify', '/classify/{doc}/{corpus}')
+    config.add_route('view_job', '/job-view/{doc}/{corpus}/job/{job}')
+    config.add_route('demo', '/demo')
+
+    config.include('eea.corpus.processing')
+
+    config.scan()
+
+    return config.make_wsgi_app()
---
+++ src/eea.corpus/eea/corpus/async.py
@@ -0,0 +1,5 @@
+from flask import Flask
+from pkg_resources import resource_filename
+from pyramid.paster import bootstrap
+from redis import Redis
+from red
