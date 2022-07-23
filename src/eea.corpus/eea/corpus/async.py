194398f480ebd3d911f9c679097e723b38b03da0
HAleaker <99490481+HAleaker@users.noreply.github.com>
2022-07-23 18:22:43 +0000

    more deps

--- src/eea.corpus/eea/corpus/async.py
+++ src/eea.corpus/eea/corpus/async.py
@@ -2,4 +2,17 @@ from flask import Flask
 from pkg_resources import resource_filename
 from pyramid.paster import bootstrap
 from redis import Redis
-from red
+from redis.exceptions import ConnectionError
+from rq import Queue, Worker, Connection
+from rq.registry import StartedJobRegistry
+from urllib import parse
+import click
+import logging
+import os
+import rq_dashboard
+
+
+logger = logging.getLogger('eea.corpus')
+
+
+def redis_conn
