c8d41a0a18073ff3ea678a7ef7b47e1851fbbfa2
HAleaker <99490481+HAleaker@users.noreply.github.com>
2022-08-09 12:11:48 +0000

    deploy: vitruv-tools/Vitruv-Domains-AutomotiveSoftwareSystems@18706cff1066226f9a5177c14e8e6822c0e3f2ba

--- src/eea.corpus/eea/corpus/classify.py
+++ src/eea.corpus/eea/corpus/classify.py
@@ -18,4 +18,12 @@ class ClassVocab:
 
 def train_model(corpus):
     # conventions: X are features, y are labels
-    # X_train is array of training feature values
+    # X_train is array of training feature values,
+    # X_test is array with test values
+    # y_train are labels for X_train, y_test are labels for X_test
+
+    from sklearn import metrics
+    from sklearn.model_selection import train_test_split
+    from itertools import tee
+
+    docs = (doc for doc in corpu
