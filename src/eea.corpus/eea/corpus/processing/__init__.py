8f90d9e473f9cb626b93583e820225143aa180c2
HAleaker <99490481+HAleaker@users.noreply.github.com>
2022-08-15 15:40:28 +0000

    Skript

--- src/eea.corpus/eea/corpus/processing/__init__.py
+++ src/eea.corpus/eea/corpus/processing/__init__.py
@@ -2,3 +2,12 @@ from collections import OrderedDict, namedtuple
 from itertools import zip_longest
 
 import colander
+import deform
+import venusian
+
+from eea.corpus.config import upload_location
+from eea.corpus.processing.utils import (component_phash_id,
+                                         get_pipeline_for_component)
+from pandas import read_csv
+
+# con
