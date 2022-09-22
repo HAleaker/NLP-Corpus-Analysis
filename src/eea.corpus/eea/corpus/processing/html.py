67955f414dbed2afe66d7b4f07d08aadcf928839
HAleaker <99490481+HAleaker@users.noreply.github.com>
2022-09-22 20:33:52 +0000

    f2fs: try to flush inode after merging inline data

--- src/eea.corpus/eea/corpus/processing/html.py
+++ src/eea.corpus/eea/corpus/processing/html.py
@@ -6,4 +6,13 @@ import logging
 from bs4 import BeautifulSoup
 from colander import Schema
 
-from eea.corpus.processing i
+from eea.corpus.processing import pipeline_component  # , needs_text_input
+from eea.corpus.utils import set_text
+
+logger = logging.getLogger('eea.corpus')
+
+
+class BeautifulSoupText(Schema):
+    """ Schema for BeautifulSoup based parser
+    """
+    description = "Uses BeautifulSoup t
