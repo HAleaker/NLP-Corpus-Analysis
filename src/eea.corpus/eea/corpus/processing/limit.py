cd54fd120f85e57d405c17a410538e165f232b75
HAleaker <99490481+HAleaker@users.noreply.github.com>
2022-10-01 19:15:33 +0000

    MAINT: save work.

--- src/eea.corpus/eea/corpus/processing/limit.py
+++ src/eea.corpus/eea/corpus/processing/limit.py
@@ -6,4 +6,16 @@ from eea.corpus.processing import pipeline_component
 from itertools import islice
 import logging
 
-logger = logging.getLogger('eea.cor
+logger = logging.getLogger('eea.corpus')
+
+
+class LimitResults(Schema):
+
+    # description = "Limit the number of processed documents"
+
+    max_count = SchemaNode(
+        Int(),
+        default=10,
+        missing=10,
+        title='Results limit',
+        description='
