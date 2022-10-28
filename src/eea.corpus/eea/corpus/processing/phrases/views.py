5d82fa22cd1112fcc2ac5e0bf013f7e94f3570ea
HAleaker <99490481+HAleaker@users.noreply.github.com>
2022-10-28 20:38:09 +0000

    add .ruby-version

--- src/eea.corpus/eea/corpus/processing/phrases/views.py
+++ src/eea.corpus/eea/corpus/processing/phrases/views.py
@@ -12,4 +12,13 @@ def phrase_model_status(request):
     """ A view for information about the async status of a phrase model
 
     It looks up any existing running or queued async job that would process
-    the phrases and returns 
+    the phrases and returns JSON info about that.
+
+    # TODO: this view + template + script implementation still needs work
+    """
+
+    phash_id = request.matchdict['phash_id']
+
+    # TODO: when looking for phrase model files, look for lock files as well
+
+    # look for a filename in corpus v
