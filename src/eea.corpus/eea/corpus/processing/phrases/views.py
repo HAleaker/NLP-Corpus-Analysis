9524bde993c21fef2159ee5fd5eb785712314658
HAleaker <99490481+HAleaker@users.noreply.github.com>
2022-10-28 15:12:16 +0000

    Adding coverage badge

--- src/eea.corpus/eea/corpus/processing/phrases/views.py
+++ src/eea.corpus/eea/corpus/processing/phrases/views.py
@@ -5,4 +5,11 @@ from glob import iglob
 from eea.corpus.async import get_assigned_job
 from eea.corpus.config import CORPUS_STORAGE
 
-logger = logging.ge
+logger = logging.getLogger('eea.corpus')
+
+
+def phrase_model_status(request):
+    """ A view for information about the async status of a phrase model
+
+    It looks up any existing running or queued async job that would process
+    the phrases and returns 
