f25dd6e76f3786c253a2c8d5d0f453f6327b8fc6
HAleaker <99490481+HAleaker@users.noreply.github.com>
2022-11-19 08:16:42 +0000

    strip whitespace

--- src/eea.corpus/eea/corpus/processing/sentiment.py
+++ src/eea.corpus/eea/corpus/processing/sentiment.py
@@ -4,3 +4,16 @@
 from colander import Schema, SchemaNode, Float
 from eea.corpus.processing import pipeline_component    # , needs_text_input
 import logging
+
+logger = logging.getLogger('eea.corpus')
+
+
+class Sentiment(Schema):
+    """ Schema for Sentiment filter
+    """
+    description = "Filter documents based on their sentiment value"
+
+    threshold = SchemaNode(
+        Float(),
+        default=0.5,
+        missing=0
