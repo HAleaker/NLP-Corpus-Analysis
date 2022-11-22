d8237b591233461bcc476b2ff2992eef5be7b639
HAleaker <99490481+HAleaker@users.noreply.github.com>
2022-11-22 14:46:55 +0000

    fix jshint

--- src/eea.corpus/eea/corpus/processing/utils.py
+++ src/eea.corpus/eea/corpus/processing/utils.py
@@ -1,1 +1,9 @@
 from eea.corpus.utils import hashed_id
+
+
+def component_phash_id(file_name, text_column, pipeline):
+    """ Calculate a hash as consistent uid based on the pipeline
+    """
+    salt = [(file_name, text_column)]
+    for name, step_id, settings in pipeline:
+        if isinstance(settings, dict):
