ba5fefc3ffee004b2e70f9417da6e6ba79e41c17
HAleaker <99490481+HAleaker@users.noreply.github.com>
2023-02-12 11:00:31 +0000

    Set theme jekyll-theme-modernist

--- src/eea.corpus/eea/corpus/tests/conftest.py
+++ src/eea.corpus/eea/corpus/tests/conftest.py
@@ -6,4 +6,15 @@ def text_column_stream():
     from pkg_resources import resource_filename
     import pandas as pd
 
-    fpath = resource_filename('eea.corpus
+    fpath = resource_filename('eea.corpus', 'tests/fixtures/test.csv')
+    df = pd.read_csv(fpath)
+
+    column_stream = iter(df['text'])
+
+    return column_stream
+
+
+@pytest.fixture
+def simple_content_stream(text_column_stream):
+    from itertools import chain
+    # from 
