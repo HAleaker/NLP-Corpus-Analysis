8941fb673d872ad3b749872d01c18ff022fd36c0
HAleaker <99490481+HAleaker@users.noreply.github.com>
2023-03-22 10:16:50 +0000

    Delete settings.json

--- src/eea.corpus/eea/corpus/tests/processing/test_html.py
+++ src/eea.corpus/eea/corpus/tests/processing/test_html.py
@@ -1,1 +1,12 @@
-from unittest.mock im
+from unittest.mock import Mock, patch
+
+
+class TestHTML:
+    texts = (
+        "<strong>Hello</strong> world",
+        "Just plain text",
+    )
+
+    def test_schema(self):
+        from eea.corpus.processing.html import BeautifulSoupText
+ 
