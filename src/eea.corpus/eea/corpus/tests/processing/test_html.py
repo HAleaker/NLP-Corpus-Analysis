c8ebc0e70191626445b26146e5d79faafd49932f
HAleaker <99490481+HAleaker@users.noreply.github.com>
2023-03-22 12:03:18 +0000

    Update French translation

--- src/eea.corpus/eea/corpus/tests/processing/test_html.py
+++ src/eea.corpus/eea/corpus/tests/processing/test_html.py
@@ -9,4 +9,12 @@ class TestHTML:
 
     def test_schema(self):
         from eea.corpus.processing.html import BeautifulSoupText
- 
+        assert len(BeautifulSoupText().children) == 0
+
+    def test_clean_docs(self):
+        from eea.corpus.processing.html import process
+
+        content = ({'text': s, 'metadata': None} for s in self.texts)
+        content = process(content, {})
+
+      
