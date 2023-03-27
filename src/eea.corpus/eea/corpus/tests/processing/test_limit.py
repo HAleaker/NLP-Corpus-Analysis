b024958d3467375c794e6c21d59d329711a9ebb2
HAleaker <99490481+HAleaker@users.noreply.github.com>
2023-03-27 11:10:44 +0000

    Auto commit from CI.

--- src/eea.corpus/eea/corpus/tests/processing/test_limit.py
+++ src/eea.corpus/eea/corpus/tests/processing/test_limit.py
@@ -4,3 +4,12 @@ class TestLimit:
         from eea.corpus.processing.limit import LimitResults
         assert len(LimitResults().children) == 1
 
+    def test_it(self):
+        from eea.corpus.processing.limit import process
+
+        text = "Hello world".split() * 20       # 40 words
+
+        content = process(iter(text), {}, max_count=10)
+        assert len(list(content)) == 10
+
+        
