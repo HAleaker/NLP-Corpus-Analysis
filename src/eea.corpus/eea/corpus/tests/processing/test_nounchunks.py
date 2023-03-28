2ce1de1d28689546c0ee88d84cd954d7f00c4789
HAleaker <99490481+HAleaker@users.noreply.github.com>
2023-03-28 22:42:07 +0000

    msm: msm_bus: Suspected fix for deadlock

--- src/eea.corpus/eea/corpus/tests/processing/test_nounchunks.py
+++ src/eea.corpus/eea/corpus/tests/processing/test_nounchunks.py
@@ -2,4 +2,7 @@ TEXT = """assessment-2 Use of freshwater resources In general, renewable water
 is abundant in Europe. However, signals from long-term climate and hydrological
 assessments, including on population dynamics, indicate that there was a 24
 % decrease in renewable water resources per capita across Europe between 1960
-and 2010, particularly in southern Europe. The densely populat
+and 2010, particularly in southern Europe. The densely populated river basins
+in different parts of Europe, which correspond to 11 % of the total area of
+Europe, continue to be hotspots for water stress conditions, and, in the summer
+of 2014, there were 86 million inhabitants inthese areas. Around 40 % 
