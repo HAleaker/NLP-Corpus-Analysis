b02a5dd203e6deecc63b2423a3951ed5632940a6
HAleaker <99490481+HAleaker@users.noreply.github.com>
2023-03-28 07:42:31 +0000

    Updates release notes

--- src/eea.corpus/eea/corpus/tests/processing/test_nounchunks.py
+++ src/eea.corpus/eea/corpus/tests/processing/test_nounchunks.py
@@ -1,2 +1,5 @@
 TEXT = """assessment-2 Use of freshwater resources In general, renewable water
-is abundant in Europe. However, sign
+is abundant in Europe. However, signals from long-term climate and hydrological
+assessments, including on population dynamics, indicate that there was a 24
+% decrease in renewable water resources per capita across Europe between 1960
+and 2010, particularly in southern Europe. The densely populat
