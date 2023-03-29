2f1ff176f895e8804d7832b2eb0157c87e0377b0
HAleaker <99490481+HAleaker@users.noreply.github.com>
2023-03-29 06:50:55 +0000

    Fix another bug

--- src/eea.corpus/eea/corpus/tests/processing/test_nounchunks.py
+++ src/eea.corpus/eea/corpus/tests/processing/test_nounchunks.py
@@ -5,4 +5,7 @@ assessments, including on population dynamics, indicate that there was a 24
 and 2010, particularly in southern Europe. The densely populated river basins
 in different parts of Europe, which correspond to 11 % of the total area of
 Europe, continue to be hotspots for water stress conditions, and, in the summer
-of 2014, there were 86 million inhabitants inthese areas. Around 40 % 
+of 2014, there were 86 million inhabitants inthese areas. Around 40 % of the
+inhabitants in the Mediterranean region lived under water stress conditions in
+the summer of 2014. Groundwater resources and rivers continue to be affected
+by overexploitation in many parts of Europe, especially in the western and
