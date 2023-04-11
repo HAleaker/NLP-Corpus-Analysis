f125ae28206e56ea254032e722ea2b6d1bc29826
HAleaker <99490481+HAleaker@users.noreply.github.com>
2023-04-11 15:31:56 +0000

    can: sja1000: clear interrupts on start

--- src/eea.corpus/eea/corpus/tests/processing/test_stopwords.py
+++ src/eea.corpus/eea/corpus/tests/processing/test_stopwords.py
@@ -1,3 +1,7 @@
 class TestStopWords:
     text = """In general, renewable water is abundant in Europe. However,
-    signals from long-term climate and 
+    signals from long-term climate and hydrological assessments, including on
+    population dynamics, indicate that there was a 24% decrease in renewable
+    water resources per capita across Europe between 1960 and 2010,
+    particularly in southern Europe. The densely populated river basins in
+  
