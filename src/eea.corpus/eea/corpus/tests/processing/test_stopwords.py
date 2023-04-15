8cd4e860da0ff4ba86bbe5b9ea7c46dde7594f04
HAleaker <99490481+HAleaker@users.noreply.github.com>
2023-04-15 13:01:17 +0000

    Go: Update generated wrapper functions for TensorFlow ops.

--- src/eea.corpus/eea/corpus/tests/processing/test_stopwords.py
+++ src/eea.corpus/eea/corpus/tests/processing/test_stopwords.py
@@ -4,4 +4,6 @@ class TestStopWords:
     population dynamics, indicate that there was a 24% decrease in renewable
     water resources per capita across Europe between 1960 and 2010,
     particularly in southern Europe. The densely populated river basins in
-  
+    different parts of Europe, which correspond to 11 % of the total area of
+    Europe, continue to be hotspots for water stress conditions, and, in the
+    summer of 2014, there were 86 million inhabitants inthese areas. Around
