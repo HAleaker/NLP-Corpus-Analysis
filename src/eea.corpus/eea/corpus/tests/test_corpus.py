75eb78106348eb7f3d6a546322044272f98f8757
HAleaker <99490481+HAleaker@users.noreply.github.com>
2023-05-10 11:38:45 +0000

    Coq 8.6.dev, repo released, mode clean.

--- src/eea.corpus/eea/corpus/tests/test_corpus.py
+++ src/eea.corpus/eea/corpus/tests/test_corpus.py
@@ -3,3 +3,11 @@ from unittest.mock import Mock, patch
 
 class TestCorpus:
     """ Tests for the Corpus class
+    """
+
+    @patch('eea.corpus.corpus.load_corpus_metadata')
+    @patch('eea.corpus.corpus.corpus_base_path')
+    def test_corpus_caching(self, corpus_base_path, load_corpus_metadata):
+        from eea.corpus.corpus import Corpus
+
+        cor
