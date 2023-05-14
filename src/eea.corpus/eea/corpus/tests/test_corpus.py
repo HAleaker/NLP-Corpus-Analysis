9374dc7709964c5c881cbb27b973d33024bf017b
HAleaker <99490481+HAleaker@users.noreply.github.com>
2023-05-14 11:48:15 +0000

    added tracking

--- src/eea.corpus/eea/corpus/tests/test_corpus.py
+++ src/eea.corpus/eea/corpus/tests/test_corpus.py
@@ -10,4 +10,13 @@ class TestCorpus:
     def test_corpus_caching(self, corpus_base_path, load_corpus_metadata):
         from eea.corpus.corpus import Corpus
 
-        cor
+        corpus = Corpus('filename', 'corpusid')
+        corpus._docs_stream = iter(range(100))
+
+        assert len(corpus._cache) == 0
+
+        x = list(corpus)
+        assert len(x) == 100
+        assert len(list(corpus)) == 100
+        assert len(list(corpus)) == 100
+        assert
