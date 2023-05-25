bfb7e09877630525fc2fc19a52d079fc02fe857f
HAleaker <99490481+HAleaker@users.noreply.github.com>
2023-05-25 17:36:09 +0000

    fiddling

--- src/eea.corpus/eea/corpus/tests/test_corpus.py
+++ src/eea.corpus/eea/corpus/tests/test_corpus.py
@@ -25,4 +25,11 @@ class TestCorpus:
     @patch('eea.corpus.corpus.load_corpus_metadata')
     @patch('eea.corpus.corpus.corpus_base_path')
     def test_corpus_metadata(self, corpus_base_path, load_corpus_metadata):
-      
+        from eea.corpus.corpus import Corpus
+
+        corpus = Corpus('a', 'b')
+        corpus._meta = {
+            'statistics': {'docs': 30},
+            'title': 'corpus title',
+            'description': 'corpus description',
+    
