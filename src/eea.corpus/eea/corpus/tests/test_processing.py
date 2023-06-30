ddf4515dfb3c2c50f4d0bc732080c09f465f8429
HAleaker <99490481+HAleaker@users.noreply.github.com>
2023-06-30 13:18:46 +0000

    Bump patch version

--- src/eea.corpus/eea/corpus/tests/test_processing.py
+++ src/eea.corpus/eea/corpus/tests/test_processing.py
@@ -14,4 +14,12 @@ class TestProcessing:
 
     @patch('eea.corpus.processing.upload_location')
     def test_build_pipeline_for_preview(self, upload_location):
-        from eea.corpus.proce
+        from eea.corpus.processing import build_pipeline
+        from pkg_resources import resource_filename
+
+        file_name = 'test.csv'
+        upload_location.return_value = resource_filename(
+            'eea.corpus', 'tests/fixtures/test.csv')
+        text_column = 'text'
+
+     
