9191ea91f037a4ae51d3a9678e97855bddbd3987
HAleaker <99490481+HAleaker@users.noreply.github.com>
2023-06-30 06:17:34 +0000

    Rename a file

--- src/eea.corpus/eea/corpus/tests/test_processing.py
+++ src/eea.corpus/eea/corpus/tests/test_processing.py
@@ -6,4 +6,12 @@ class TestProcessing:
     @classmethod
     def setup_class(cls):
         cls.config = testing.setUp()
-        cls.config.scan('
+        cls.config.scan('eea.corpus.processing')
+
+    @classmethod
+    def teardown_class(cls):
+        testing.tearDown()
+
+    @patch('eea.corpus.processing.upload_location')
+    def test_build_pipeline_for_preview(self, upload_location):
+        from eea.corpus.proce
