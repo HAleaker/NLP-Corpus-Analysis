32ce32da28cf46d9a6408207a1ef9d08e0b13a1e
HAleaker <99490481+HAleaker@users.noreply.github.com>
2023-07-28 11:20:29 +0000

    overall_stats: make dual/quad core stats configurable via meunconfig

--- src/eea.corpus/eea/corpus/utils.py
+++ src/eea.corpus/eea/corpus/utils.py
@@ -1,1 +1,16 @@
-from __f
+from __future__ import unicode_literals
+
+import hashlib
+import logging
+import os
+import random
+import string
+
+from cytoolz import compose
+from eea.corpus.config import CORPUS_STORAGE
+
+logger = logging.getLogger('eea.corpus')
+
+
+def rand(n):
+    re
